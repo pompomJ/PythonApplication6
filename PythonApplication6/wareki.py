e6475be9ebb332fef4d7e7453acefef11f314bce
ZamaVerce2 <Verce@LAPTOP-8JD0V3Q5>
2021-11-15 09:00:29 +0900

    プロジェクト ファイルを追加します。

---
+++ PythonApplication6/wareki.py
@@ -0,0 +1,23 @@
+year = int(input("西暦："))
+
+if year == 1868:
+    print("明治元年")
+elif year < 1912:
+    print("明治", year - 1867,"年")
+elif year == 1912:
+    print("大正元年")
+elif year < 1926:
+    print("大正", year - 1911,"年")
+elif year == 1926:
+    print("昭和元年")
+elif year < 1989:
+    print("昭和", year - 1925,"年")
+elif year == 1989:
+    print("平成元年")
+elif year == 2019:
+    print("令和元年")
+else:
+    print("令和", year - 2018, "年")
+
+input()
+
